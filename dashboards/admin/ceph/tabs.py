00284ed3deb19453095ab8524bf97fb6d2f52e80
Matt Weeks <maweeks@cisco.com>
2014-04-09 00:52:52 -0700

    add overview tab

---
+++ dashboards/admin/ceph/tabs.py
@@ -0,0 +1,20 @@
+from django.core.urlresolvers import reverse  # noqa
+from django.utils.translation import ugettext_lazy as _  # noqa
+
+from horizon import exceptions
+from horizon import tabs
+
+from openstack_dashboard import api
+
+
+class OverviewTab(tabs.Tab):
+    name = _("Overview")
+    slug = "overview"
+    template_name = "admin/ceph/_overview.html"
+
+    def get_context_data(self, request):
+        return None
+
+class CephTabs(tabs.TabGroup):
+    slug = "ceph_tabs"
+    tabs = (OverviewTab,)
--- dashboards/admin/ceph/views.py
+++ dashboards/admin/ceph/views.py
@@ -1,8 +1,10 @@
-from horizon import views
+from horizon import tabs
+from .tabs import CephTabs
 
 
-class IndexView(views.APIView):
+class IndexView(tabs.TabView):
     # A very simple class-based view...
+    tab_group_class = CephTabs
     template_name = 'admin/ceph/index.html'
 
     def get_data(self, request, context, *args, **kwargs):
